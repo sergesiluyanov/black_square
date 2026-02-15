44d92d3395a23eed0f3ed1b5887933633e937db7
sergey <sergey@kinescope.io>
2026-02-15 11:03:36 +0400

    update TURN server credentials in Config for enhanced WebRTC connectivity

--- lib/config.dart
+++ lib/config.dart
@@ -7,5 +7,5 @@ class Config {
   /// Пароль должен совпадать с тем, что в /etc/turnserver.conf на сервере.
   static const String? turnUrl = 'turn:213.171.27.44:3478';
   static const String? turnUsername = 'blacksquare';
-  static const String? turnCredential = 'CHANGE_ME'; // Замените после настройки coturn
+  static const String? turnCredential = 'BS_turn_9f0c4bfebe13e8f1';
 }
